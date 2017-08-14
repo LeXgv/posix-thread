370697df2b7a9725b9e4eb28f6384bd43db4bff9
aleksey <alekseu147147@yandex.ru>
2017-08-14 16:48:01 +0300

    t

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <pthread.h>
 #include <iostream>
 #include <sys/types.h>
-#include <unystd.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <stdio.h>
+#include <string.h>
 void *thread_func(void *arg)
 {
 std::cout << "Запущен новый поток\n";
@@ -16,8 +18,10 @@ int main()
 
 std::cout << "программа запущена/n";
 pid_t pid = getpid();
-int fd = open("main.pid", O_CREATE | O_EXCL | O_TRUNC, 0777);
-write(fd, pid, sizeof(pid));
+int fd = open("main.pid", O_CREAT | O_TRUNC | O_RDWR, 0777);
+char buf[50] = {0};
+snprintf(buf, 50, "%d", (int)pid);
+write(fd,buf, strlen(buf));
 std::cout << pid << std::endl;
 close(fd);
 pthread_t p_id;
